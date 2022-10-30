81a27dad1c4619c147160be64937f0d35386dd25
Sebastião Willian <swillian2009@gmail.com>
2022-10-30 16:22:17 -0300

    Correção das pastas dos exercícios - Continuação.

--- ExercíciosJavaScript/GrassHopperExercises - Nível 1.js
+++ ExercíciosJavaScript/GrassHopperExercises - Nível 1.js
@@ -64,22 +64,3 @@ for (var element of otherBackpack.equipment) {
         print('I found some rope!');
     }
 };
-
-// Encontrar a quantidade de caracteres de uma string. Acesso pelo método lenght da classe.
-print(message);
-if (message.length > 80) {
-    print('The message is too long for a postcard.');
-}
-if (message.length < 80) {
-    print('The message fits on a postcard');
-};
-
-// O método includes() determina se um array/objeto contém um determinado elemento ou não, retornando true ou false, respectivamente. 
-// Em outras palavras, includes() retornará true se o elemento existir no array/objeto.
-print(chat);
-if (chat.includes('hello')) {
-    print('Hi, can I help you pick a destination?');
-}
-if (chat.includes('bye')) {
-    print('See you later, have a great trip!');
-}
