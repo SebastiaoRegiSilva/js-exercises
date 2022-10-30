f3b49173da18544edd5b02a4ef983c269102aba5
Sebastião Willian <swillian2009@gmail.com>
2022-10-30 14:45:03 -0300

    Exercício de laço aninhado.

---
+++ novo.js
@@ -0,0 +1,19 @@
+// Exercícios GrassHopper.
+// Laço aninhado.
+
+// Para cada.
+for (var adj of [
+    'Raging',
+    'Happy',
+    'Hungry'
+]) {
+    for (var noun of [
+        'Pharmacists',
+        'Squids',
+        'Twins'
+    ]) {
+        print('The ' + adj + ' ' + noun);
+    }
+}
+
+// Os laços aninhados farão todas as combinações das 2 arrays.
