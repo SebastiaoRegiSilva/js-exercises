700191bf8c7005e9b3a7d80e50f1cc4b61f41c4f
Sebastião Willian <swillian2009@gmail.com>
2022-10-30 16:18:50 -0300

    EJ - Utilizando o método includes.

--- novo.js
+++ novo.js
@@ -65,7 +65,7 @@ for (var element of otherBackpack.equipment) {
     }
 };
 
-// Encontrar a quantidade de caracteres de uma string. Acesso pela propriedade da classe.
+// Encontrar a quantidade de caracteres de uma string. Acesso pelo método lenght da classe.
 print(message);
 if (message.length > 80) {
     print('The message is too long for a postcard.');
@@ -73,3 +73,13 @@ if (message.length > 80) {
 if (message.length < 80) {
     print('The message fits on a postcard');
 };
+
+// O método includes() determina se um array/objeto contém um determinado elemento ou não, retornando true ou false, respectivamente. 
+// Em outras palavras, includes() retornará true se o elemento existir no array/objeto.
+print(chat);
+if (chat.includes('hello')) {
+    print('Hi, can I help you pick a destination?');
+}
+if (chat.includes('bye')) {
+    print('See you later, have a great trip!');
+}
