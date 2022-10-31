895b1423e6ff278b8390f27546a9f79d5097404e
Sebastião Willian <swillian2009@gmail.com>
2022-10-31 13:50:33 -0300

    EJ - Uso da função Slice().

--- ExercíciosJavaScript/GrassHopperExercises - Nível 2.js
+++ ExercíciosJavaScript/GrassHopperExercises - Nível 2.js
@@ -148,3 +148,21 @@ myAssistant('10am', turnOffFan);
 import { listOfDestinations } from 'grasshopper.travel';
 let numberOfDestinations = listOfDestinations.length;
 console.log('There are ' + numberOfDestinations + ' to choose from.');
+
+// Uso da função .slice();
+// O método .slice() cria uma nova array copiando uma subseção de outra array. 
+// São necessários 2 argumentos: um índice de array onde começar a copiar e um índice onde terminar (fim não incluído).
+import { transportation } from 'grasshopper.travel';
+let cities = [
+    'London',
+    'London',
+    'London',
+    'Lagos',
+    'Lisbon',
+    'Louisville',
+    ___
+];
+console.log(cities.slice(2, 6));
+console.log(transportation.slice(2, 6));
+
+
