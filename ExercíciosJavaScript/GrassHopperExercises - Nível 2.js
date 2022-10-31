6673964f81d9d37902d700d95f9853851ded9537
Sebastião Willian <swillian2009@gmail.com>
2022-10-31 13:59:29 -0300

    EJ - Uso do método .pop().

--- ExercíciosJavaScript/GrassHopperExercises - Nível 2.js
+++ ExercíciosJavaScript/GrassHopperExercises - Nível 2.js
@@ -180,6 +180,15 @@ travelToCities.push(boatAtlanta);
 last = travelToCities[travelToCities.length - 1];
 console.log(last.destination);
 
+// Uso do método .pop().
+// O método .pop() removerá o último item em uma array.
+import { travelToCities } from 'grasshopper.travel';
+let atlantaTravel = travelToCities.pop();
+atlantaTravel.transportMode = 'hovercraft';
+travelToCities.push(atlantaTravel);
+let last1 = travelToCities[travelToCities.length - 1];
+console.log(last.destination);
+console.log(last.transportMode);
 
 
 
