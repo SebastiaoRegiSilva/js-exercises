8be206243a1e619f424c65457d089e310b35f270
Sebastião Willian <swillian2009@gmail.com>
2022-10-31 14:23:38 -0300

    EJ  - Manipulando dados com API e o método .getData().

--- ExercíciosJavaScript/GrassHopperExercises - Nível 2.js
+++ ExercíciosJavaScript/GrassHopperExercises - Nível 2.js
@@ -234,7 +234,17 @@ flightPrices.forEach(compareToMinimum);
 console.log('Maximum: ' + max);
 console.log('Minimum: ' + min);
 
-
+// Uso da função .getData() para uso de API grasshopper.reviews.
+import { getData, findHotels } from 'grasshopper.reviews';
+function printHotel(hotel, ___) {
+    console.log('___HOTEL_INFO___');
+    console.log('type: ' + hotel.type);
+    console.log('city: ' + hotel.city);
+    console.log('price: ' + hotel.price);
+    console.log('rating: ' + hotel.rating);
+};
+let grasslandHotels = getData('Grassland', findHotels, ___);
+grasslandHotels.forEach(printHotel);
 
 
 
