68351e91361aa6559307213ffbf8054f054faf8c
Sebastião Willian <swillian2009@gmail.com>
2022-10-31 14:14:03 -0300

    EJ - Uso do método .filter().

--- ExercíciosJavaScript/GrassHopperExercises - Nível 2.js
+++ ExercíciosJavaScript/GrassHopperExercises - Nível 2.js
@@ -202,6 +202,22 @@ for (let time of departures) {
 }
 console.log(departures.length + ' total times');
 
+// Método .filter()
+// .filter() para selecionar itens de uma array que passam em um determinado teste.
+// Cada item na array é usado como um argumento em uma função retorno de chamada 
+// e se retornar true, o item passa pelo filtro. Caso contrário, o item é filtrado.
+import { departures } from 'grasshopper.travel';
+function morning(time, ___) {
+    return time.includes('am');
+};
+function evening(time, ___) {
+    return time.includes('pm');
+};
+let amTimes = departures.filter(morning);
+let pmTimes = departures.filter(evening);
+console.log('Day times: ' + amTimes);
+console.log('Night times: ' + pmTimes);
+
 
 
 
